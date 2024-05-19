4a69bce245c3607b64493a8ea6ef9647b9d45399
zacvong11 <von24001@byui.edu>
2024-05-18 23:09:56 -0600

    updated

--- scripts/place.js
+++ scripts/place.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Function to calculate wind chill
+
     function calculateWindChill(temp, windSpeed) {
         if (temp <= 10 && windSpeed > 4.8) {
             return (13.12 + 0.6215 * temp - 11.37 * Math.pow(windSpeed, 0.16) + 0.3965 * temp * Math.pow(windSpeed, 0.16)).toFixed(1) + "°C";
@@ -8,17 +8,17 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     }
 
-    // Set wind chill value
-    const temp = 10; // Example temperature in Celsius
-    const windSpeed = 5; // Example wind speed in km/h
+
+    const temp = 10;
+    const windSpeed = 5;
     const windChillElement = document.getElementById("windchill");
     windChillElement.textContent = calculateWindChill(temp, windSpeed);
 
-    // Set the current year in the footer
+
     const currentYear = new Date().getFullYear();
     document.getElementById('year').textContent = `©️ ${currentYear}, Zac Vongphakdy, USA`;
 
-    // Set last modified date in the footer
+  
     const lastModifiedDate = new Date(document.lastModified);
     document.getElementById('lastModified').textContent = `Last modified: ${lastModifiedDate.toLocaleString()}`;
 });
