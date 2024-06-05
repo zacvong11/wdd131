5b746cb21864813dab25807b74f3b882bdf2795b
zacvong11 <von24001@byui.edu>
2024-06-04 21:38:19 -0600

    updated css, fixed mobile view, added h1 title and last modified js

--- project/project.js
+++ project/project.js
@@ -48,3 +48,11 @@ document.addEventListener('DOMContentLoaded', function () {
     autoRotate = setInterval(showNextImage, 3000);
     updateGallery();
 });
+const currentYear = new Date().getFullYear();
+
+const footer = document.querySelector('footer');
+footer.querySelector('#year').textContent = `©️${currentYear}, Zac Vongphakdy, USA`;
+
+const lastModifiedDate = new Date(document.lastModified);
+
+document.getElementById('lastModified').textContent = `Last modified: ${lastModifiedDate}`;
